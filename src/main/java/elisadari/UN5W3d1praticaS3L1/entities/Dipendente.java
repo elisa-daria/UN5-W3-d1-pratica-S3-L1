cef5020ab12bfcffe6429fd3746d08ff473b6096
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-16 15:57:03 +0200

    Dipendente implements-UserDetails

--- src/main/java/elisadari/UN5W3d1praticaS3L1/entities/Dipendente.java
+++ src/main/java/elisadari/UN5W3d1praticaS3L1/entities/Dipendente.java
@@ -8,7 +8,10 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -16,7 +19,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @JsonIgnoreProperties({"pw,typeOfDipendente,dispositivi"})
-public class Dipendente {
+public class Dipendente implements UserDetails {
     @Id
     @GeneratedValue
     @Setter(AccessLevel.NONE)
@@ -38,5 +41,35 @@ public class Dipendente {
         this.email = email;
         this.pw = pw;
         this.typeOfDipendente = TypeOfDipendente.USER;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
